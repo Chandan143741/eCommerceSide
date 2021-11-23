22b561213b33199351d5f625d72e7b5ba5e839f9
Chandan143741 <60169200+Chandan143741@users.noreply.github.com>
2021-11-23 16:38:10 +0530

    Added Entity Framework

---
+++ eCommerceSide/eCom.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using eCom.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCom.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
