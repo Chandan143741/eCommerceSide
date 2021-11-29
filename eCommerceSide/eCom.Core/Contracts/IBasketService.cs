f57197d528be4a979025db6bc3faa911dd4f3f36
Chandan143741 <60169200+Chandan143741@users.noreply.github.com>
2021-11-30 00:58:30 +0530

    Finished Basket Service

---
+++ eCommerceSide/eCom.Core/Contracts/IBasketService.cs
@@ -0,0 +1,18 @@
+﻿using eCom.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace eCom.Core.Contracts
+{
+    public interface IBasketService
+    {
+        void AddToBasket(HttpContextBase httpContext, string productId);
+        void RemoveFromBasket(HttpContextBase httpContext, string itemId);
+        List<BasketItemViewModel> GetBasketsItems(HttpContextBase httpContext);
+        BasketSummaryViewModel GetBasketSummary(HttpContextBase httpContext);
+    }
+}
